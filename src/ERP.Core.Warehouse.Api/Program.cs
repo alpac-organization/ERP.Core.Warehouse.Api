3b20bb0b94e636679b79ce5b4aa7835c9f61e6f5
David Cortes <davidcortes0209@gmail.com>
2026-07-07 16:10:43 -0600

    refreshing

--- src/ERP.Core.Warehouse.Api/Program.cs
+++ src/ERP.Core.Warehouse.Api/Program.cs
@@ -3,7 +3,6 @@ using Microsoft.OpenApi;
 using ERP.Core.Warehouse.Api.Application;
 using ERP.Core.Manager.Api.Infrastructure;
 using System.Text.Json.Serialization;
-using ERP.Core.Manager.Api.Infrastructure.Middlewares;
 using ERP.Core.Infrastructure.Middlewares;
 
 
--- src/Infrastructure/Attributes/HasTokenAttribute.cs
+++
@@ -1,6 +0,0 @@
-namespace ERP.Core.Manager.Api.Infrastructure.Attributes
-{
-    // Este atributo solo sirve para marcar qué endpoints queremos proteger
-    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class)]
-    public class HasTokenAttribute : Attribute { }
-}
