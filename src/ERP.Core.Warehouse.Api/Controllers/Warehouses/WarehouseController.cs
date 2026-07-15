b5bc37ab1ec25cb86feaa8880a41c41d23736fb3
VASSALLI-JVCO <desarrollo@vassalli.com.ni>
2026-07-15 11:32:56 -0600

    feat: agregar avances de registros de almacenes

--- src/ERP.Core.Warehouse.Api/Controllers/Warehouses/WarehouseController.cs
+++ src/ERP.Core.Warehouse.Api/Controllers/Warehouses/WarehouseController.cs
@@ -1,24 +1,31 @@
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using ERP.Core.Domain.Entities.Errors;
+using ERP.Core.Infrastructure.Attributes;
 using ERP.Core.Warehouse.Api.Controllers.ApiBase;
+using ERP.Core.Warehouse.Api.Application.Features.Warehouses.v1.Commands;
 
 namespace ERP.Core.Warehouse.Api.Controllers.Warehouses
 {
+    [HasToken]
     [ApiVersion("1.0")]
     [Route("api/v1/")]
-    public class WarehouseController(/*IMediator _mediator*/) : ApiControllerBase
+    public class WarehouseController(IMediator _mediator) : ApiControllerBase
     {
         [Tags("Almacenes")] 
         [HttpPost("companies/{company_id}/modules/{module_code}/warehouse")]      
-        [ProducesResponseType(typeof(decimal), StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(CreatedResult), StatusCodes.Status201Created)]
         [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status400BadRequest)]  
         [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status500InternalServerError)]  
-        public async Task<CreatedResult> RegisterWarehouseAsync([FromRoute] Guid company_id, [FromRoute] string module_code)
+        public async Task<CreatedResult> RegisterWarehouseAsync([FromRoute] Guid company_id, [FromRoute] string module_code, [FromBody] RegisterWarehouseCommand payload)
         {
             var userIdStr = HttpContext.Items["UserId"] as string;
 
-            //Your Code here
+            payload.CompanyId = company_id;            
+            payload.ModuleCode = module_code;
+            payload.UserId = Guid.Parse(userIdStr ?? "");
+            
+            await _mediator.Send(payload);
 
             return Created();
         }
