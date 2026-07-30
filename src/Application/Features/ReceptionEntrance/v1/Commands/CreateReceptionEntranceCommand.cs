af528d8e3be7977e5b06d5c0f0bb43f83f700be8
David Cortes <davidcortes0209@gmail.com>
2026-07-30 11:58:42 -0600

    resolved conflicts after merge

--- src/Application/Features/ReceptionEntrance/v1/Commands/CreateReceptionEntranceCommand.cs
+++ src/Application/Features/ReceptionEntrance/v1/Commands/CreateReceptionEntranceCommand.cs
@@ -1,6 +1,6 @@
 using MediatR;
-using ERP.Core.Warehouse.Api.Domain.Entities.Bases;
 using ERP.Core.Manager.Api.Domain.Enums;
+using ERP.Core.Warehouse.Api.Domain.Entities.Bases;
 
 namespace ERP.Core.Warehouse.Api.Application.Features.ReceptionEntrance.v1.Commands;
 
