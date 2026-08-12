868ccaafdf9cd04eeed49d7246ef16f36ea3fd18
David Cortes <davidcortes0209@gmail.com>
2026-08-12 14:49:40 -0600

    cambio de string a enum

--- src/Application/Features/Warehouses/v1/Dtos/SectionDto.cs
+++ src/Application/Features/Warehouses/v1/Dtos/SectionDto.cs
@@ -1,3 +1,5 @@
+using ERP.Core.Database.Domain.Enums;
+
 namespace ERP.Core.Warehouse.Api.Application.Features.Warehouses.v1.Dtos
 {
     public class SectionDto
@@ -5,7 +7,7 @@ namespace ERP.Core.Warehouse.Api.Application.Features.Warehouses.v1.Dtos
         public Guid SectionId { get; set; }
         public string? SectionCode { get; set; }
         public string? SectionName { get; set; }
-        public string? SectionType { get; set; }
+        public SectionType? SectionType { get; set; }
         public bool IsActive { get; set; }
     }
 }
