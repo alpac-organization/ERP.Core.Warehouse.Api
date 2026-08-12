a70ce70b94bb96170bcdff9cfbd99e93e2e639b4
David Cortes <davidcortes0209@gmail.com>
2026-08-12 08:39:48 -0600

    refactor(#126): endpoint Get

--- src/Application/Commons/Mappings/WarehouseProfile.cs
+++ src/Application/Commons/Mappings/WarehouseProfile.cs
@@ -12,9 +12,11 @@ namespace ERP.Core.Warehouse.Api.Application.Commons.Mappings
         public WarehouseProfile()
         {
             CreateMap<Warehouses, WarehouseDto>()
-                .ForMember(dest => dest.WarehouseId, opt => opt.MapFrom(src => src.Id))
-                .ForMember(dest => dest.WarehouseCode, opt => opt.MapFrom(src => src.Code))
-                .ForMember(dest => dest.WarehouseName, opt => opt.MapFrom(src => src.WarehouseName));
+            .ForMember(dest => dest.WarehouseId, opt => opt.MapFrom(src => src.Id))
+            .ForMember(dest => dest.WarehouseCode, opt => opt.MapFrom(src => src.Code))
+            .ForMember(dest => dest.WarehouseName, opt => opt.MapFrom(src => src.WarehouseName))
+            .ForMember(dest => dest.WarehouseType, opt => opt.MapFrom(src => src.WarehouseType.ToString()))
+            .ForMember(dest => dest.SubWarehouses, opt => opt.MapFrom(src => src.SubWarehouses));
         }
     }
 
@@ -27,42 +29,42 @@ namespace ERP.Core.Warehouse.Api.Application.Commons.Mappings
             var details = new WarehouseDetails
             {
                 Id = Guid.NewGuid(),
-                WarehouseId         = warehouseId,
-                WitdhMetres         = command.WarehouseDetails.WidthMetres,
-                LengthMetres        = command.WarehouseDetails.LengthMetres,
-                RampsCount          = command.WarehouseDetails.RampsCount,
-                ParkingSpacesCount  = command.WarehouseDetails.ParkingSpacesCount
+                WarehouseId = warehouseId,
+                WitdhMetres = command.WarehouseDetails.WidthMetres,
+                LengthMetres = command.WarehouseDetails.LengthMetres,
+                RampsCount = command.WarehouseDetails.RampsCount,
+                ParkingSpacesCount = command.WarehouseDetails.ParkingSpacesCount
             };
 
             return new()
             {
-                Id                  = warehouseId,
-                Code                = command.Code,
-                IsActive            = true,
-                WarehouseName       = command.WarehouseName,
-                BranchId            = command.BranchId,
-                WarehouseType       = command.WarehouseType,
-                ParentWarehouseId   = command.ParentWarehouseId,
+                Id = warehouseId,
+                Code = command.Code,
+                IsActive = true,
+                WarehouseName = command.WarehouseName,
+                BranchId = command.BranchId,
+                WarehouseType = command.WarehouseType,
+                ParentWarehouseId = command.ParentWarehouseId,
 
-                Details             = details,
-                Capacity            = details.ToCalculatedCapacity(warehouseId)
+                Details = details,
+                Capacity = details.ToCalculatedCapacity(warehouseId)
             };
         }
 
         public static WarehouseCapacity ToCalculatedCapacity(this WarehouseDetails details, Guid warehouseId)
         {
-            var totalArea   = details.WitdhMetres * details.LengthMetres;
+            var totalArea = details.WitdhMetres * details.LengthMetres;
 
             return new WarehouseCapacity
             {
-                Id                      = Guid.NewGuid(),
-                WarehouseId             = warehouseId,
-                TotalAreaM2             = totalArea,
-                UsableAreaM2            = null, // aún no hay secciones/racks para descontar
-                UnusableAreaM2          = null,
-                TotalMaxPolines         = null,
-                CurrentPolinesStored    = null,
-                LastCalculatedAt        = DateTime.UtcNow
+                Id = Guid.NewGuid(),
+                WarehouseId = warehouseId,
+                TotalAreaM2 = totalArea,
+                UsableAreaM2 = null, // aún no hay secciones/racks para descontar
+                UnusableAreaM2 = null,
+                TotalMaxPolines = null,
+                CurrentPolinesStored = null,
+                LastCalculatedAt = DateTime.UtcNow
             };
         }
 
