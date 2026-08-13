acd65bd12ce676d5611d6b1094b9f27b89b5489b
David Cortes <davidcortes0209@gmail.com>
2026-08-13 14:05:22 -0600

    agregar storage type a get

--- src/Application/Features/Warehouses/v1/Dtos/SectionDto.cs
+++ src/Application/Features/Warehouses/v1/Dtos/SectionDto.cs
@@ -8,6 +8,7 @@ namespace ERP.Core.Warehouse.Api.Application.Features.Warehouses.v1.Dtos
         public string? SectionCode { get; set; }
         public string? SectionName { get; set; }
         public SectionType? SectionType { get; set; }
+        public SectionStorageType? StorageType { get; set; }
         public bool IsActive { get; set; }
     }
 }
