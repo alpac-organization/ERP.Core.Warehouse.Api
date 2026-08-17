0f5816c53b714d985c51e442c8dd4ed813a9d709
Angel-Delg <lariosangel062@gmail.com>
2026-08-17 16:12:04 -0600

    feat(#167): resolved: #167 agregar filtros nuevos para solicitud de compras

--- src/Application/Features/PurchaseRequests/v1/Queries/GetPurchaseRequests.Query.cs
+++ src/Application/Features/PurchaseRequests/v1/Queries/GetPurchaseRequests.Query.cs
@@ -10,7 +10,10 @@ namespace ERP.Core.Warehouse.Api.Application.Features.PurchaseRequests.v1.Querie
         public string? Code { get; set; }
         public Guid? AreaId { get; set; }
         public Guid? BranchId { get; set; }
+
+        public PriorityLevel? PriorityLevel { get; set; }
         public PurchaseRequestStatus? Status { get; set; }
+        public DestinationRequest? Destination { get; set; }
         public PurchaseRequestType? RequestType { get; set; }
 
         public int PageSize { get; set; }
