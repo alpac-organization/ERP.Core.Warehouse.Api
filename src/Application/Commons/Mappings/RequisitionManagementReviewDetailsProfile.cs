3c002e0ee18b185d99af6375a694517ea223fd15
Angel-Delg <lariosangel062@gmail.com>
2026-08-26 15:26:26 -0600

    feat: guardar avances

---
+++ src/Application/Commons/Mappings/RequisitionManagementReviewDetailsProfile.cs
@@ -0,0 +1,29 @@
+using AutoMapper;
+using ERP.Core.Database.Domain.Entities.Shopping;
+using ERP.Core.Warehouse.Api.Application.Features.RequisitionManagementReviews.v1.Dtos;
+
+namespace ERP.Core.Warehouse.Api.Application.Commons.Mappings
+{
+    public class RequisitionManagementReviewDetailsProfile : Profile
+    {
+        public RequisitionManagementReviewDetailsProfile()
+        {
+             CreateMap<RequisitionManagementReview, RequisitionManagementReviewDetailsDto>()
+                .ForMember(dest => dest.RequisitionManagementReviewId, opt => opt.MapFrom(src => src.Id))
+                
+                .ForPath(dest => dest.SentByUserInformation, opt => opt.MapFrom(src => src.SentByUser))
+                .ForPath(dest => dest.SentByUserInformation.WorkAreaInformation, opt => opt.MapFrom(src => src.SentByUser.WorkArea))
+
+                .ForPath(dest => dest.ReviewerUserInformation, opt => opt.MapFrom(src => src.ReviewedByUser))
+
+                .ForPath(dest => dest.PurchaseRequestDetails, opt => opt.MapFrom(src => src.PurchaseRequest))
+                .ForPath(dest => dest.PurchaseRequestDetails.BranchInformation, opt => opt.MapFrom(src => src.PurchaseRequest.Branch))
+
+                
+                .ForPath(dest => dest.PurchaseRequestDetails.CreatorUserInformation, opt => opt.MapFrom(src => src.PurchaseRequest.RegistrationUser))
+                .ForPath(dest => dest.PurchaseRequestDetails.CreatorUserInformation.WorkAreaInformation, opt => opt.MapFrom(src => src.PurchaseRequest.RegistrationUser.WorkArea))
+                
+                ;
+        }
+    }
+}
---
+++ src/Application/Features/RequisitionManagementReviews/v1/Handlers/GetRequisitionManagementReviewsDetails.Handler.cs
@@ -0,0 +1,52 @@
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+
+using ERP.Core.Application.Commons.Interfaces;
+using ERP.Core.Database.Application.Commons.Interfaces.Bases;
+using ERP.Core.Database.Application.Commons.Interfaces.Repositories;
+
+using ERP.Core.Warehouse.Api.Application.Features.RequisitionManagementReviews.v1.Dtos;
+using ERP.Core.Warehouse.Api.Application.Features.RequisitionManagementReviews.v1.Queries;
+
+namespace ERP.Core.Warehouse.Api.Application.Features.RequisitionManagementReviews.v1.Handlers
+{
+    public class GetRequisitionManagementReviewsDetailsHandler(IUnitOfWork _unitOfWork, IErrorManager _errorManager, IMapper _mapper) :  BaseValidatorHandler<GetRequisitionManagementReviewsDetailsQuery, RequisitionManagementReviewDetailsDto>(_unitOfWork, _errorManager)
+    {
+        public override async Task<RequisitionManagementReviewDetailsDto> Handle(GetRequisitionManagementReviewsDetailsQuery request, CancellationToken cancellationToken)
+        {
+            var access = await ValidateAccessAsync(request.UserId, request.CompanyId, request.ModuleCode!, cancellationToken);
+
+            if (!access.IsSuccess)
+            {
+                return access.ErrorResponse!;
+            }
+
+            var reviewsQuery = await _unitOfWork.RequisitionManagementReviews.Entities
+                .Include(rev => rev.SentByUser)
+                    .ThenInclude(user => user.WorkArea)
+
+                .Include(rev => rev.PurchaseRequest)
+                    .ThenInclude(rev => rev.PurchaseRequestItems)
+
+                .Include(rev => rev.PurchaseRequest)
+                    .ThenInclude(rev => rev.RegistrationUser)
+                        .ThenInclude(rev => rev.WorkArea)
+
+                .Include(rev => rev.PurchaseRequest)
+                    .ThenInclude(rev => rev.Branch)
+
+                .Include(rev => rev.PurchaseRequest)
+                    .ThenInclude(rev => rev.UserRevision)
+
+                .Include(rev => rev.PurchaseRequest)
+                    .ThenInclude(rev => rev.WorkArea)
+
+                .Where(review => review.Id == request.RequisitionManagementReviewsId)
+                .FirstOrDefaultAsync(cancellationToken);
+
+            var mapped =  _mapper.Map<RequisitionManagementReviewDetailsDto>(reviewsQuery);
+
+            return mapped;
+        }
+    }
+}
