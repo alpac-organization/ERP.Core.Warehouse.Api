e30db040cfec5838c27e6d4882edeaa055d91b21
David Cortes <davidcortes0209@gmail.com>
2026-09-10 11:21:24 -0600

    fix de mapeo de id de lots

--- src/Application/Features/Warehouses/v1/Dtos/LotsDto.cs
+++ src/Application/Features/Warehouses/v1/Dtos/LotsDto.cs
@@ -4,7 +4,7 @@ using ERP.Core.Database.Domain.Enums;
 
 public class LotListItemDto
 {
-    public Guid Id { get; set; }
+    public Guid LotId { get; set; }
     public string Code { get; set; } = null!;
     public RackStatus Status { get; set; }
     public bool AllowsStacking { get; set; }
