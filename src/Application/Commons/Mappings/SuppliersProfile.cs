4589c61499f87be55cac82c72ba6f482bba19641
Angel-Delg <lariosangel062@gmail.com>
2026-09-24 16:01:39 -0600

    feat: agregar cuentas de bancos al peo de información de proveedor

--- src/Application/Commons/Mappings/SuppliersProfile.cs
+++ src/Application/Commons/Mappings/SuppliersProfile.cs
@@ -1,5 +1,4 @@
 using AutoMapper;
-using ERP.Core.Database.Domain.Entities.Catalogs;
 using ERP.Core.Database.Domain.Entities.Shopping;
 using ERP.Core.Warehouse.Api.Application.Features.PurchaseRequests.v1.Dtos;
 
@@ -10,7 +9,10 @@ namespace ERP.Core.Warehouse.Api.Application.Commons.Mappings
         public SuppliersProfile()
         {
             CreateMap<Supplier, SupplierInformation>()
-                .ForMember(dest => dest.SupplierId,    opt => opt.MapFrom(src => src.Id));
+                .ForMember(dest => dest.SupplierId,    opt => opt.MapFrom(src => src.Id))
+                .ForPath(dest => dest.SupplierDetailsInformation,    opt => opt.MapFrom(src => src.SupplierDetails));
+
+            CreateMap<SupplierDetails, SupplierDetailsInformation>();
         }
     }
 }
--- src/Application/Features/PurchaseRequests/v1/Dtos/PurchaseRequestItemDto.cs
+++ src/Application/Features/PurchaseRequests/v1/Dtos/PurchaseRequestItemDto.cs
@@ -49,8 +49,9 @@ namespace ERP.Core.Warehouse.Api.Application.Features.PurchaseRequests.v1.Dtos
         public bool IsActive { get; set; }
         public bool HasDelivery { get; set; }
         public bool HasGuarantee { get; set; }
+        public bool InventoryAvailable { get; set; }
         public bool IsAcceptedForPurchase { get; set; }
-
+        
         public decimal Iva { get; set; }
         public decimal Price { get; set; }
         public decimal PriceUnit { get; set; }
@@ -68,6 +69,12 @@ namespace ERP.Core.Warehouse.Api.Application.Features.PurchaseRequests.v1.Dtos
         public string? SupplierSelectionJustification { get; set; }
         public string? SupplierRejectionJustification { get; set; }
 
+        public ProductQuality ProductQuality { get; set; }
+        public PaymentCondition PaymentCondition { get; set; }
+
+        public decimal? AvailabilityTime { get; set; }
+        public TimeType? AvailabilityTimeType { get; set; }
+
         public Guid SupplierId { get; set; }
         public SupplierInformation SupplierInformation { get; set; } = new();
     }
@@ -79,5 +86,36 @@ namespace ERP.Core.Warehouse.Api.Application.Features.PurchaseRequests.v1.Dtos
         public string? SuppliersLegalName { get; set; }
         public string? IdentificationNumber { get; set; }
         public IdentificationType? IdentificationType { get; set; }
+        public SupplierDetailsInformation SupplierDetailsInformation { get; set; } = new();
+        public List<SupplierBankAccounts> SupplierBankAccounts { get; set; } = [];
+    }
+
+    public class SupplierBankAccounts
+    {
+        public Currency Currency { get; set; }
+        public BankAccountType AccountType { get; set; }
+        public string BankName { get; set; } = string.Empty;
+        public string AccountNumber { get; set; } = string.Empty;
+        public string AccountHolderName { get; set; } = string.Empty;
+        public string? AccountHolderIdentification { get; set; }
+        public bool IsPrimary { get; set; } = false;
+    }
+
+    public class SupplierDetailsInformation
+    {
+        public bool IsExclusive { get; set; }
+        public string? ExclusiveBrandsOrParts { get; set; }
+
+        public int CreditDays { get; set; }
+        public bool HasCredit { get; set; }
+
+        public decimal? CreditLimit { get; set; }
+        public int AlertDaysBeforeDue { get; set; }
+        public Currency? CreditCurrency { get; set; }
+        public PaymentMethodType PreferredPaymentMethod { get; set; }
+
+        public bool ApplyIrRetention { get; set; }
+        public bool ApplyMunicipalRetention { get; set; }
+        public bool IsTaxExempt { get; set; }
     }
 }
